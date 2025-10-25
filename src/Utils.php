bdf1e209510bdcc9c33f25b7aadf0e858f950f98
murYG <mur@jinr.ru>
2025-10-25 22:08:05 +0300

    test fixtures

--- src/Utils.php
+++ src/Utils.php
@@ -5,5 +5,5 @@ namespace Hexlet\Phpunit\Utils;
 // Эта функция переворачивает переданную строку
 function reverseString(string $string): string
 {
-    return implode(array_reverse(str_split($string)));
+    return implode(array_reverse(mb_str_split($string)));
 }
--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -18,5 +18,13 @@ class UtilsTest extends TestCase
         // И только потом актуальное (actual)
         $this->assertEquals('', reverseString(''));
         $this->assertEquals('olleh', reverseString('hello'));
+		
+		$this->assertStringEqualsFile($this->getFixtureFullPath('fishtext.txt'), reverseString('Значимость этих проблем настолько очевидна, что постоянный количественный рост и сфера нашей активности способствует подготовки и реализации систем массового участия. Равным образом сложившаяся структура организации требуют от нас анализа новых предложений. Не следует, однако забывать, что сложившаяся структура организации позволяет выполнять важные задания по разработке существенных финансовых и административных условий. Разнообразный и богатый опыт реализация намеченных плановых заданий позволяет оценить значение новых предложений.'));
     }
+	
+	public function getFixtureFullPath(string $fixtureName): string
+	{
+		$parts = [__DIR__, 'fixtures', $fixtureName];
+		return realpath(implode('/', $parts));
+	}	
 }
